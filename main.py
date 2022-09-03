402e3d6240a28f40b628ac664fa1a93e2ed9c33b
Jaheim007 <jaheimkouaho@gmail.com>
2022-09-03 15:46:26 +0000

    Function_4

---
+++ main.py
@@ -0,0 +1,13 @@
+def main(name):
+    vowels = ["a" , "e" , "i" , "o" , "u"]
+    
+    print(name)
+    
+    if vowels in name: 
+        print("vowels")  
+
+    print("constant")
+    
+main("Jaheim")
+    
+    
